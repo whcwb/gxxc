70bc140fca39259acf8ee6373fe3b3bce415b1a5
slu <384920400@qq.com>
2018-05-17 18:24:49 +0800

    1

--- biz/src/main/java/com/cwb/platform/biz/controller/HdController.java
+++ biz/src/main/java/com/cwb/platform/biz/controller/HdController.java
@@ -1,16 +1,19 @@
 package com.cwb.platform.biz.controller;
 
+import com.cwb.platform.util.bean.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cwb.platform.biz.model.BizHd;
 import com.cwb.platform.biz.service.HdService;
 import com.cwb.platform.sys.base.BaseController;
 import com.cwb.platform.sys.base.BaseService;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/hd")
+@RequestMapping("/pub/hd")
 public class HdController extends BaseController<BizHd,java.lang.String>{
     @Autowired
     private HdService service;
@@ -19,4 +22,16 @@ public class HdController extends BaseController<BizHd,java.lang.String>{
     protected BaseService<BizHd, java.lang.String> getBaseService() {
         return service;
     }
+
+    @Override
+    @RequestMapping(value="/save", method={RequestMethod.POST})
+    public ApiResponse<String> save(BizHd entity){
+        return service.saveEntity(entity);
+    }
+    @Override
+    @RequestMapping(value="/update", method={RequestMethod.POST})
+    public ApiResponse<String> update(BizHd entity){
+        return service.updateEntity(entity);
+    }
+
 }
--- biz/src/main/java/com/cwb/platform/biz/service/HdService.java
+++ biz/src/main/java/com/cwb/platform/biz/service/HdService.java
@@ -3,7 +3,11 @@ package com.cwb.platform.biz.service;
 
 import com.cwb.platform.biz.model.BizHd;
 import com.cwb.platform.sys.base.BaseService;
+import com.cwb.platform.util.bean.ApiResponse;
 
 public interface HdService extends BaseService<BizHd,java.lang.String>{
-    
+
+    ApiResponse<String> saveEntity(BizHd entity);
+
+    ApiResponse<String> updateEntity(BizHd entity);
 }
