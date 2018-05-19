b53abdbc3585d027104db849993a5a7090393516
yaingx <udjpyud@163.com>
2018-05-19 13:35:35 +0800

    修改引用对象

--- biz/src/main/java/com/cwb/platform/biz/controller/PtyhController.java
+++ biz/src/main/java/com/cwb/platform/biz/controller/PtyhController.java
@@ -1,13 +1,12 @@
 package com.cwb.platform.biz.controller;
 
-import com.cwb.platform.util.bean.ApiResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import com.cwb.platform.biz.model.BizPtyh;
 import com.cwb.platform.biz.service.PtyhService;
 import com.cwb.platform.sys.base.BaseController;
 import com.cwb.platform.sys.base.BaseService;
+import com.cwb.platform.sys.model.BizPtyh;
+import com.cwb.platform.util.bean.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/ptyh")
