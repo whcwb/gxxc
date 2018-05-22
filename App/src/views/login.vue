c4b0fb54d8aff4864e0aaa497638e57b9861c4a2
quan <1538452228@qq.com>
2018-05-22 18:08:38 +0800

    login

--- App/src/views/login.vue
+++ App/src/views/login.vue
@@ -2,30 +2,50 @@
 @import  "./login.less";
 </style>
 <template>
-<div id="login">
-    <div class="bobo">
 
+<div id="login" style="background-size:100%;background: url('../../static/bg.png') no-repeat center;height: 100%">
+  <div style="font-size: 0.5rem;text-align: center;margin-bottom: 0.8rem;color: white">登录</div>
+  <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
+  <Row>
+    <Col span="24" >
       <md-field>
-        <md-input-item
-          ref="input0"
-          title="账号"
-          placeholder="手机号"
-          :maxlength="5"
-        ></md-input-item>
-        <md-input-item
-          ref="input0"
-          title="密码"
-          placeholder="请输入您的密码"
-          :maxlength="5"
-        ></md-input-item>
+        <div >
+          <Icon type="ios-person-outline" size="30" style="float: left;margin-top: 25px;margin-right: 10px;"></Icon>
+          <md-input-item  style="float: left" ref="name" title="真实姓名"  placeholder="投保人姓名"  is-title-latent clearable >
+          </md-input-item>
+        </div>
+        <div>
+          <Icon type="ios-locked-outline" size="30" style="float: left;margin-top: 25px;margin-right: 10px;"></Icon>
+          <md-input-item
+            ref="id"
+            title="身份证号"
+            placeholder="投保人身份证号"
+            is-title-latent
+            clearable
+          ></md-input-item>
+        </div>
+
       </md-field>
+    </Col>
+  </Row>
+
+
+<div style="padding: 0.2rem;margin-top: 0.8rem;" class="but">
+
+  <Button  type="success" long @click="handleSubmit('formInline')">登录</Button>
+</div>
+    <div style="padding: 0.2rem" class="but">
+      <Button type="error" long> 注册</Button>
     </div>
+
+  </Form>
+
 </div>
 </template>
 
 <script>
-  import {InputItem, Field} from 'mand-mobile'
-
+  import {InputItem, Field,} from 'mand-mobile'
+  import {Button,Icon,Form,FormItem ,Input ,Row,Col} from 'iview'
   export default {
     name: 'input-item-demo',
     /* DELETE */
@@ -34,7 +54,38 @@
     components: {
       [InputItem.name]: InputItem,
       [Field.name]: Field,
+      Button,
+      Icon,Form,FormItem ,Input,Row,Col
     },
+    data () {
+      return {
+        formInline: {
+          user: '',
+          password: ''
+        },
+        ruleInline: {
+          user: [
+            { required: true, message: '请输入账号', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      handleSubmit(name) {
+        this.$refs[name].validate((valid) => {
+          if (valid) {
+            this.$Message.success('Success!');
+          } else {
+            this.$Message.error('Fail!');
+          }
+        })
+      }
+    }
   }
 
 </script>
+
