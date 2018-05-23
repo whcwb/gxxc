51012f82fab5fdd5919ea93f53d56d56c6a776dd
libinbin <libinbin@rhtop.cn>
2018-05-23 09:53:50 +0800

    web

---
+++ web/src/axios/api.js
@@ -0,0 +1,156 @@
+//网络请求地址前缀
+let BASE_URL = 'http://127.0.0.1';
+module.exports = {
+    url: BASE_URL,
+    //图片上传地址
+    UPLOAD: BASE_URL + '/upload',
+    STATIC_PATH: BASE_URL + ':8765/server/',
+    VIDEO_PATH:'',
+    USERROOT:{
+        GET_MENU_LIST:'/api/gn/getUserFunctions',
+        GET_MENU_TREE:'/api/gn/getMenuTree',
+        INIT_MENU:'/api/gn/initMenu',
+        MODIFY_PSD:'/api/yh/mdfPwd'
+    },
+    LOGIN:{
+        QUERY:'login'
+    },
+    //文件控制
+    FILE:{
+        FINDBYPID:'/api/files/findByPId',//根据主ID查询文件信息
+        UPLOAD:BASE_URL + '/api/files/upload',//根据主ID查询文件信息
+    },
+    USER:{
+        QUERY:'/api/yh/pager',//用户管理
+        ADD:'/api/yh/save',
+        CHANGE:'/api/yh/update',
+        GIVE:'/api/js/modifyUserRoles',
+        DELE:'/api/yh/removeIds',
+        SEARCHSOME:'/api/yh/query',//用户管理
+    },
+    ROLE:{
+        QUERY:'/api/js/pager',//角色管理
+        ALL:'/api/js/getAll',//角色管理
+        ADD:'/api/js/save',
+        CHANGE:'/api/js/update',
+        GIVE:'/api/js/modifyUserRoles',
+        DELE:'/api/js/removeIds',
+        MODIFY_USER_ROLES:'/api/js/modifyUserRoles',
+        GET_USER_ROLES:'/api/js/getUserRoles'
+    },
+    FRAMEWORK:{
+        QUERY:'api/jg/pager',//机构管理
+        ADD:'/api/jg/save',
+        CHANGE:'/api/jg/update',
+        DELE:'/api/jg/removeIds',
+        GET_TREE:'/api/jg/getOrgTree',
+        GET_TREE_Node:'/api/jg/getTree',
+        getSubOrgList:'/api/jg/getSubOrgList',
+    },
+    DICTIONARY:{
+        QUERY:'/api/zd/pager',// 查询字典
+        ADD:'/api/zd/save',// 新增字典
+        CHANGE:'/api/zd/update',// 编辑字典
+        DELE:'/api/zd/removeIds' // 删除字典
+    },
+    DICTIONARY_LIST:{
+        QUERY:'/api/zdxm/pager',//查询字典项
+        ADD:'/api/zdxm/save',// 新增字典项
+        CHANGE:'/api/zdxm/update',// 编辑字典项
+        DELE:'/api/zdxm/removeIds',// 删除字典项
+        GET_BY_CONDITION:'/api/zdxm/getCondition', // 删除字典
+    },
+    ITMS:{
+        QUERY:'api/fw/pager',//服务管理
+        ADD:'/api/fw/save',
+        CHANGE:'/api/fw/update',
+        DELE:'/api/fw/removeIds'
+    },
+    FUNCTION:{
+        QUERY:'api/gn/pager',//功能管理
+        ADD:'/api/gn/save',
+        CHANGE:'/api/gn/update',
+        DELE:'/api/gn/removeIds',
+        GET_ORG_PERMISSION_TREE:'/api/gn/getOrgPermissionTree',
+        GET_ALL_PERMISSION_TREE:'/api/gn/getAllPermissionTree',
+        GET_ROLE_PERMISSION_TREE:'/api/gn/getRolePermissionTree',
+        SET_ROLE_FUNCTIONS:'/api/gn/setRoleFunctions',
+        SET_ORG_FUNCTIONS:'/api/gn/setOrgFunctions',
+        GET_ROLE_FUNCTIONS:'/api/gn/getRoleFunctions'
+    },
+    DAILY:{
+        QUERY:'api/rz/pager',//日志管理
+        ADD:'/api/rz/save',
+        CHANGE:'/api/rz/update',
+        DELE:'/api/rz/removeIds'
+    },
+    OIL_CARD:{
+        QUERY:'api/oilCard/pager',//油卡管理
+        ADD:'/api/oilCard/save',
+        CHANGE:'/api/oilCard/update',
+        CHARGE:'/api/oilCard/charge',
+        DELE:'/api/oilCard/removeIds',
+    },
+    CAR:{
+        QUERY:'api/car/pager',//车辆管理
+        SEARCHSOME:'/api/car/query',//指定条件查询数据
+        ADD:'/api/car/save',
+        countByCondition:'/api/car/countByCondition',
+        lqnjCarList:'/api/car/lqnjCarList',
+        CHANGE:'/api/car/update',
+        DELE:'/api/car/removeIds',
+        ALLOCPERSON:'/api/car/allocPerson',
+        notUseCarList:'/api/car/notUseCarList',
+        CLNS:'/api/car/clnsUpdate',
+    },
+    illegal:{
+        QUERY:'api/illegal/pager',//违法管理
+        ADD:'/api/illegal/save',
+        CHANGE:'/api/illegal/update',
+        DELE:'/api/illegal/removeIds',
+    },
+    maintain:{
+        QUERY:'api/maintain/pager',//保养管理
+        ADD:'/api/maintain/save',
+        CHANGE:'/api/maintain/update',
+        DELE:'/api/maintain/removeIds',
+    },
+    maintainInfo:{
+        QUERY:'api/maintainInfo/pager',//保养管理
+        ADD:'/api/maintainInfo/save',
+        CHANGE:'/api/maintainInfo/update',
+        DELE:'/api/maintainInfo/removeIds',
+    },
+    insurance:{
+        QUERY:'api/insurance/pager',//保险管理
+        ADD:'/api/insurance/save',
+        CHANGE:'/api/insurance/update',
+        DELE:'/api/insurance/removeIds',
+    },
+    userCar:{
+        QUERY:'api/userCar/pager',//出车管理
+        ADD:'/api/userCar/save',
+        CHANGE:'/api/userCar/update',
+        DELE:'/api/userCar/removeIds',
+        getLastReturnMileage:'/api/userCar/getLastReturnMileage',
+    },
+    repair:{
+        QUERY:'api/repair/pager',//维修管理
+        ADD:'/api/repair/save',
+        CHANGE:'/api/repair/update',
+        DELE:'/api/repair/removeIds',
+    },
+    repairInfo:{
+        QUERY:'api/repairInfo/pager',//维修管理
+        ADD:'/api/repairInfo/save',
+        CHANGE:'/api/repairInfo/update',
+        DELE:'/api/repairInfo/removeIds',
+    },
+    oilRecord:{
+        QUERY:'api/oilRecord/pager',//油料管理
+        list:'/api/oilRecord/list',
+        ADD:'/api/oilRecord/save',
+        CHANGE:'/api/oilRecord/update',
+        DELE:'/api/oilRecord/removeIds',
+    },
+}
---
+++ web/src/views/whdx/system/system-dictionary/index.vue
@@ -0,0 +1,215 @@
+<style lang="less">
+	@import '../../../../styles/common.less';
+	.bodyWidth{
+		box-shadow: 2px 5px 5px #888888
+	}
+</style>
+<!--字典管理-->
+<template>
+	<div class="boxbackborder box" style="background-color: #fff;">
+		<Row style="padding-bottom: 16px;">
+			<div  style="display: inline-block">
+				<label class="searchLabel">字典名称:</label>
+				<Input v-model="findMess.lmmcLike" placeholder="请输入字典名称..."
+					   style="width: 200px"
+					   @on-keyup.enter="findMessList()"
+					   @on-change="findMessList"></Input>
+			</div>
+			<Button type="primary" @click="findMessList()">
+				<Icon type="search"></Icon>
+				<!--查询-->
+			</Button>
+			<Button type="primary" @click="AddDc()">
+				<Icon type="plus-round"></Icon>
+			</Button>
+		</Row>
+		<div class="body padding-auto margin-5">
+			<div class="box-row-list">
+				<div class="bodyWidth" v-for="(item,index) in dictionary" :key='index'>
+					<Card style="width:100%">
+						<p slot="title">
+							<Icon type="ios-film-outline"></Icon>
+							{{item.lmmc}} [{{item.lmdm}}]
+						</p>
+						<span slot="extra">
+					        <a href="#" @click.prevent="AddDcList(item,index)">
+					            <Icon type="plus-circled" size="24"></Icon>
+								<!--新增-->
+					        </a>
+					        <a href="#" @click.prevent="removeDc(item,index)" style="color: red;">
+					            <Icon type="close-circled" size="24"></Icon>
+								<!--删除-->
+					        </a>
+				        </span>
+						<div style="height: 240px;">
+							<div>
+								<Row class="margin-bottom-10">
+									<Input v-model="dictionaryMess[index]" clearable placeholder="请输入字典信息..." @on-change="findDicList(item,index,dictionaryMess[index])"></Input>
+								</Row>
+								<div class="padding-2px-5px box-row-nh" style='background-color: #f8f8f9;text-align: center;'>
+									<div class="body-2" style="min-width: 30px;">
+										序
+									</div>
+									<div class="body-8">
+										类目编码
+									</div>
+									<div class="body-8">
+										类目名称
+									</div>
+									<div class="body-2" style="min-width: 50px;">
+										操作
+									</div>
+								</div>
+							</div>
+							<div style="height: 180px;overflow: auto;">
+								<div class="box-row-nh margin-bottom-5" v-for='(items,indexs) in item.zdxmList'  style='text-align: center;'>
+									<div class="body-2" style="min-width: 30px;">
+										{{(indexs+1)}}
+									</div>
+									<div class="body-8">
+										{{items.zddm}}
+									</div>
+									<div class="body-8">
+										{{items.zdmc}}
+									</div>
+									<div class="body-2" style="min-width: 50px;">
+										<Button type="error" size='small' shape="circle" icon="close" @click="removeDcList(item,items)"></Button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</Card>
+				</div>
+			</div>
+			<Row class="margin-top-10 pageSty">
+				<!--<Page :total=pageTotal :current=page.pageNum :page-size=page.pageSize show-total show-elevator @on-change='pageChange'></Page>-->
+			</Row>
+		</div>
+		<component :is="compName" :dicListMess='dicListMess'></component>
+	</div>
+</template>
+
+<script>
+	import mixins from '@/mixins'
+
+
+	import addmess from './comp/addmess.vue'
+	import addmessList from './comp/addmessList.vue'
+	import mess from './comp/mess.vue'
+	export default {
+		name: 'char',
+		mixins: [mixins],
+		components: {
+			addmessList,
+			addmess,
+			mess
+		},
+		data() {
+			return {
+				dicListMess: '',
+				SpinShow: true,
+				compName: '',
+				dictionaryMess: [], //字典信息查找
+				//分页
+				pageTotal: 1,
+				page: {
+					pageNum: 1,
+					pageSize:8
+				},
+				dictionary: [],
+				//收索
+				findMess: {
+					gte_StartTime: '',
+					lte_StartTime: '',
+					lmmcLike: '',
+					like_ScName: '',
+					pageNum: 1,
+					pageSize: 150
+				}
+			}
+		},
+		created() {
+				this.SpinShow = false;
+			this.getmess()
+		},
+		methods: {
+			getmess() {
+				var v = this
+				this.$http.post(this.apis.DICTIONARY.QUERY , this.findMess).then((res) => {
+					log('字典数据', res)
+					v.dictionary = res.page.list
+					this.SpinShow = false;
+				})
+			},
+			colsemodal() {
+				this.compName = ''
+			},
+			AddDc() {
+				var v = this
+				v.compName = 'addmess'
+			},
+			findMessList(mess) {
+				var v = this
+				this.$http.post(this.apis.DICTIONARY.QUERY, this.findMess).then((res) => {
+					log('字典数据', res)
+					v.dictionary = res.page.list
+				})
+			},
+			removeDc(item, index) {
+				this.util.del(this,this.apis.DICTIONARY.DELE,[item.lmdm],()=>{
+                    this.getmess();
+				});
+			},
+			AddDcList(item, index) {
+				var v = this
+				v.compName = 'addmessList'
+				v.dicListMess = item.lmdm
+			},
+			findDicList(item, index, mess) {
+				var v = this
+				this.$http.post(this.apis.DICTIONARY_LIST.QUERY, {
+					'zdlmdm': item.lmdm,
+					'zdmcLike': mess
+				}).then((res) => {
+					log('字典数据', res)
+					if(res.code === 200) {
+						v.dictionary[index].zdxmList = res.page.list
+					}
+					return
+				})
+			},
+			removeDcList(item, items) {
+				var v = this
+				swal({
+				  title: "是删除数据?",
+				  text: "",
+				  icon: "warning",
+				  buttons:['取消','确认'],
+				})
+				.then((willDelete) => {
+				  if (willDelete) {
+					v.$http.post(this.apis.DICTIONARY_LIST.DELE, {'ids': [items.zdId]}).then((res) =>{
+						if(res.code===200){
+							this.$Message.success(res.message);
+						}else{
+							this.$Message.warning(res.message);
+						}
+						v.getmess()
+					})
+				  } else {
+				  }
+				});
+			},
+			changrDcList(item, items) {
+				var v = this
+				v.compName = 'addmessList'
+				v.dicListMess = '你好啊！'
+			},
+			pageChange(event) {
+				var v = this
+				v.page.pageNum = event
+				v.AddDc(v.page)
+			}
+		}
+	}
+</script>
