9b1d445ef03b636ba1d5aab17bf38b1751f172b6
Lee <Lee@DESKTOP-JVNT571>
2018-05-28 09:04:25 +0800

    提交

---
+++ mob-page/src/views/home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="home">
+    <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
+      <router-link to="/" slot="left">
+        <md-icon name="arrow-left" size="lg"></md-icon>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+    <md-button  type="ghost" size="small" disabled style="margin-left:5px">Primary</md-button>
+    <!--<md-button @click="handleClick">点我</md-button>-->
+    <mt-tabbar>
+      <mt-tab-item id="外卖">
+        外卖
+      </mt-tab-item>
+      <mt-tab-item id="订单">
+        订单
+      </mt-tab-item>
+      <mt-tab-item id="发现">
+        发现
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+</template>
+
+<script>
+  import { Button, Toast, Icon } from 'mand-mobile'
+  import { Header, Tabbar, TabItem } from 'mint-ui';
+
+  export default {
+    name: 'home-view',
+    components: {
+      [Icon.name]: Icon,
+      [Button.name]: Button,
+      [Header.name]: Header,
+      [Tabbar.name]: Tabbar,
+      [TabItem.name]: TabItem,
+    },
+    methods: {
+      handleClick() {
+        Toast.info('不错哟~')
+      }
+    }
+  }
+</script>
