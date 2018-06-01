52631a3606373eba4b934070ac38618bf83577c9
libinbin <libinbin@rhtop.cn>
2018-06-01 18:17:32 +0800

    端口

--- web/src/views/whdx/components/formItems.vue
+++ web/src/views/whdx/components/formItems.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
-        <Col v-for="(i,index) in formInputs" :span="i.span ? i.span : 12" :key="index">
-            <FormItem :prop='i.prop' :label='i.label'>
-                <Input v-if="!i.type || i.type ==='text'" v-model="formItem[i.prop]" :placeholder="'请填写'+i.label+'...'" :readonly="readonly && i.readonly" :disabled="readonly && i.disabled"></Input>
+        <Col v-for="(i,index) in formInputs" :span="i.separator ? 24 : (i.span ? i.span : 12)" :key="index">
+            <div v-if="i.separator && i.label"  style="text-align: center">
+                <h3>{{i.label}}</h3>
+            </div>
+            <div v-if="i.separator" style="border-bottom: 1px solid #e9eaec;height: 2px;text-align: center;margin-top: 1px;margin-bottom: 16px;"></div>
+            <FormItem v-else :prop='i.prop' :label='i.label'>
+                <Input v-if="!i.dict &&(!i.type || i.type ==='text')" v-model="formItem[i.prop]" :placeholder="'请填写'+i.label+'...'" :readonly="readonly && i.readonly" :disabled="readonly && i.disabled">
+                    <span v-if="i.prepend" slot="prepend">{{i.prepend}}</span>
+                    <span v-if="i.append" slot="append">{{i.append}}</span>
+                </Input>
                 <InputNumber v-else-if="i.type ==='number'" v-model="formItem[i.prop]" :placeholder="'请填写'+i.label+'...'" :readonly="readonly && i.readonly" :disabled="readonly && i.disabled"></InputNumber>
                 <DatePicker v-else-if="i.type == 'date'"  :value="formItem[i.prop]" type="date" placeholder="请选择日期" @on-change="(date)=>{formItem[i.prop] = date}"  :readonly="readonly && i.readonly"  :disabled="readonly && i.disabled"></DatePicker>
                 <DatePicker v-else-if="i.type == 'datetime'"  :value="formItem[i.prop]" type="datetime" placeholder="请选择日期" @on-change="(date)=>{formItem[i.prop] = date}"  :readonly="readonly && i.readonly" :disabled="readonly && i.disabled"></DatePicker>
-                <Select v-else-if="i.type === 'dict'" filterable clearable  v-model="formItem[i.prop]" :placeholder="'请选择'+i.label+'...'" :readonly="readonly && i.readonly" :disabled="readonly && i.disabled">
+                <Select v-else-if="i.dict || i.type === 'dict'" filterable clearable  v-model="formItem[i.prop]" :placeholder="'请选择'+i.label+'...'" :readonly="readonly && i.readonly" :disabled="readonly && i.disabled">
                     <Option v-for = '(item,index) in parent.dictUtil.getByCode(parent,i.dict)' :value="item.key" :key="item.key">{{item.val}}</Option>
                 </Select>
                 <Select v-else-if="i.type === 'foreignKey'" :disabled="readonly && i.disabled" filterable clearable  v-model="formItem[i.prop]" :placeholder="'请选择'+i.label+'...'">
--- web/src/views/whdx/tx/audit.vue
+++ web/src/views/whdx/tx/audit.vue
@@ -6,7 +6,7 @@
 </style>
 <template>
 	<div>
-		<Modal v-model="showModal" width='900' :closable='false'
+		<Modal v-model="showModal" width='1200' :closable='false'
         			:mask-closable="false" :title="operate+''">
         	<div style="overflow: auto;height: 500px;">
         		<Form ref="form"
@@ -31,29 +31,27 @@
 	import formItems from '../components/formItems'
 	export default {
 		name: 'txForm',
+        components:{formItems},
 		data() {
 			return {
 			    v:this,
-                operate:'新建',
-		        components:{formItems},
+                operate:'审核',
+				saveUrl:this.apis.tx.audit,
 				showModal: true,
 				readonly: false,
 				formItem: {
 				},
                 formInputs:[
+                    {separator:true,label:'基本信息'},
                     {label:'用户名称',prop:'yhMc'},
-                    {label:'提现方式',prop:'ttFs',dict:'ZDCLK0047'},
-                    {label:'提现金额(单位分)',prop:'ttJe'},
-                    {label:'提现时间',prop:'ttSj'},
-                    {label:'提现状态',prop:'ttZt',dict:'ZDCLK0048'},
-                    {label:'红包发送的次数',prop:'ttHbcs'},
-                    {label:'提现红包凭证 (用于查询红包是否已经被领取)',prop:'ttHbpz'},
-                    {label:'审核人',prop:'ttShr'},
-                    {label:'提现审核状态',prop:'ttShzt',dict:'ZDCLK0049'},
-                    {label:'备注',prop:'ttBz'},
+                    {label:'提现金额',prop:'ttJe',append:'分'},
                     {label:'银行卡号',prop:'ttYhkh'},
                     {label:'开户行',prop:'ttKhh'},
+                    {label:'提现方式',prop:'ttFs',dict:'ZDCLK0047'},
                     {label:'提现姓名',prop:'ttXm'},
+                    {separator:true,label:'审核结果'},
+                    {label:'提现审核状态',prop:'ttShzt',dict:'ZDCLK0049'},
+                    {label:'备注',prop:'ttBz'},
                 ],
                 ruleInline:{
 				}
