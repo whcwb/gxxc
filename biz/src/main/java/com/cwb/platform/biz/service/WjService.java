c3535cc1d1627b4f87ff8d2016a821e505897431
yaingx <udjpyud@163.com>
2018-06-19 18:54:05 +0800

    身份证识别

--- biz/src/main/java/com/cwb/platform/biz/service/WjService.java
+++ biz/src/main/java/com/cwb/platform/biz/service/WjService.java
@@ -4,6 +4,9 @@ package com.cwb.platform.biz.service;
 import com.cwb.platform.biz.model.BizWj;
 import com.cwb.platform.sys.base.BaseService;
 
+import java.util.Map;
+
 public interface WjService extends BaseService<BizWj,java.lang.String>{
-    
+
+    void ocrRecognition(Map<String, String> retMap, String fileType,String filePath);
 }
