eea882d06304f1a9edd699e9d83ecf68d3cb4665
chenwei <544486090@qq.com>
2018-06-21 20:48:35 +0800

    微信推送

--- app0.2/src/views/main/coach/list.vue
+++ app0.2/src/views/main/coach/list.vue
@@ -1,38 +1,48 @@
+<style scoped>
+  .dropdown_menu{
+    width: 33%;
+    display: inline-block;
+  }
+  i{
+    font-size: 14px;
+  }
+</style>
 <template>
   <div>
-    <div class="head">
-      <span>教练推荐</span>
+    <div class="_head" style="height: 40px;text-align: center">
+      <i style="font-size: 14px">教练推荐</i>
     </div>
-    <div>
-      <div>
-        <md-drop-menu
-          ref="dropMenu1"
-          :data="data1"
-        ></md-drop-menu>
-      </div>
-      <div>
-        <md-drop-menu
-          ref="dropMenu2"
-          :data="data2"
-        ></md-drop-menu>
-      </div>
-      <div>
-        <md-drop-menu
-          ref="dropMenu3"
-          :data="data3"
-        ></md-drop-menu>
-      </div>
-    </div>
-    <div class="list">
-      <div class="item">
-        <div class="left">
-
+    <div style="clear: both"></div>
+    <!--<div>-->
+      <!--<div >-->
+        <!--<md-drop-menu-->
+          <!--ref="dropMenu1"-->
+          <!--:data="data"-->
+        <!--&gt;</md-drop-menu>-->
+      <!--</div>-->
+    <!--</div>-->
+    <div class="">
+      <div class="" style=";border-bottom: 1px solid #eee">
+        <div class="left" style="width: 30%;float: left">
+          <img src="/static/png/tab-default.png" style="width: 100%">
         </div>
-        <div class="mid">
-
-        </div>
-        <div class="right">
-
+        <div class="mid" style="width: 70%;float: left">
+          <div class="mid_up">
+            <i>赵教练（12年教龄）</i>
+            <i class="el-icon-circle-check-outline" style="color: #00a854"></i>
+          </div>
+          <div class="mid_mid">
+            <el-rate
+              v-model="coach.jlPf"
+              disabled
+              show-score
+              text-color="#ff9900"
+              score-template="{value}">
+            </el-rate>
+          </div>
+          <div class="mid_down">
+            <i>天天驾校欢迎您</i>
+          </div>
         </div>
       </div>
     </div>
@@ -44,22 +54,56 @@
   import { DropMenu } from 'mand-mobile'
     export default {
         name: "list",
+      components: {
+        [DropMenu.name]: DropMenu,
+      },
       data(){
           return {
-            data1: [
+            coach:{jlPf:5},
+            data: [
               {
-                text: '一级选项1',
+                text: '排量',
                 options: [
                   {
-                    text: '二级选项1',
+                    text: '1.6L',
                   },
                   {
-                    text: '二级选项2',
+                    text: '1.8L',
+                  },
+                  {
+                    text: '2.0L',
+                  },
+                  {
+                    text: '1.2T',
+                  },
+                  {
+                    text: '1.4T',
+                  },
+                  {
+                    text: '1.6T',
                   },
                 ],
-              }
-              ],
+              },
+              {
+                text: '变速箱',
+                options: [
+                  {
+                    text: '手动挡',
+                  },
+                  {
+                    text: '自动挡',
+                  },
+                  {
+                    text: '手自一体',
+                  },
+                ],
+              },
+            ],
+
           }
+      },
+      created(){
+        this.util.auto(window, document ,7.5)
       }
     }
 </script>
