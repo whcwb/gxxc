aacfd792615b79131074797b69ee9e4915287ca2
chenwei <544486090@qq.com>
2018-06-25 11:40:05 +0800

    微信菜单

--- biz/src/main/java/com/cwb/platform/biz/wxpkg/handler/MenuHandler.java
+++ biz/src/main/java/com/cwb/platform/biz/wxpkg/handler/MenuHandler.java
@@ -44,39 +44,11 @@ public class MenuHandler extends AbstractHandler {
 	 */
 	public WxMpXmlOutNewsMessage getNewsMenu(WxMpXmlMessage wxMessage){
 		WxMpXmlOutNewsMessage.Item item = new WxMpXmlOutNewsMessage.Item();
-		item.setDescription("我的设备");
-		item.setPicUrl("http://"+domainUrl+"/weixin/wx_logo.jpg");
-		item.setTitle("我的设备");
-		item.setUrl("http://"+domainUrl+"/weixin/#/map?openid="+wxMessage.getFromUser());
-		
-		WxMpXmlOutNewsMessage.Item wfjb = new WxMpXmlOutNewsMessage.Item();
-		wfjb.setDescription("违法举报");
-		wfjb.setTitle("违法举报");
-		wfjb.setUrl("http://"+domainUrl+"/weixin/#/ssp?openid="+wxMessage.getFromUser()+"&ret_url=ssp");
-		
-		WxMpXmlOutNewsMessage.Item ssp = new WxMpXmlOutNewsMessage.Item();
-		ssp.setDescription("随手拍绑定");
-		ssp.setTitle("随手拍绑定");
-		ssp.setUrl("http://"+domainUrl+"/weixin/#/bindSsp?openid="+wxMessage.getFromUser()+"&ret_url=bindSsp");
-		
-		WxMpXmlOutNewsMessage.Item photo = new WxMpXmlOutNewsMessage.Item();
-		photo.setDescription("云相册");
-		photo.setTitle("云相册");
-		photo.setUrl("http://"+domainUrl+"/weixin/#/photo?openid="+wxMessage.getFromUser()+"&ret_url=photo");
-		
-		WxMpXmlOutNewsMessage.Item video = new WxMpXmlOutNewsMessage.Item();
-		video.setDescription("云视频");
-		video.setTitle("云视频");
-		video.setUrl("http://"+domainUrl+"/weixin/#/video?openid="+wxMessage.getFromUser()+"&ret_url=video");
 
 		WxMpXmlOutNewsMessage m = WxMpXmlOutMessage.NEWS()
 		  .fromUser(wxMessage.getToUser())
 		  .toUser(wxMessage.getFromUser())
 		  .addArticle(item)
-		  .addArticle(wfjb)
-		  .addArticle(ssp)
-		  .addArticle(photo)
-		  .addArticle(video)
 		  .build();
 		
 		return m;
