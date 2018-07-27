8db3ae25e977f4fa8356af33adf35f64f332306d
yaingx <udjpyud@163.com>
2018-07-27 18:29:15 +0800

    定时任务-业务

--- biz/src/main/java/com/cwb/platform/biz/mapper/BizTjDzMapper.java
+++ biz/src/main/java/com/cwb/platform/biz/mapper/BizTjDzMapper.java
@@ -14,4 +14,17 @@ public interface BizTjDzMapper extends Mapper<BizTjDz> {
             ",#{bizTjDz.creationTime}   " +
             " </script>"})
     void addStatistics(@Param("bizTjDz") BizTjDz bizTjDz, @Param("billDate") String billDate);
+
+    @Update({ "<script> " +
+            " UPDATE biz_tj_dz SET DZ_SJ = #{bizTjDz.dzSj} , ORACLE_COUNT = #{bizTjDz.oracleCount} " +
+            " ,ORACLE_FEE=#{bizTjDz.oracleFee} ,BILL_COUNT =#{bizTjDz.billCount} ,BILL_FEE= #{bizTjDz.billFee} " +
+            "  ,SUCCESS_COUNT=#{bizTjDz.successCount} " +
+            "  ,SUCCESS_FEE=#{bizTjDz.successFee} " +
+            "  ,SUCCESS_REFUND_COUNT=#{bizTjDz.successRefundCount} " +
+            "  ,SUCCESS_REFUND_FEE=#{bizTjDz.successRefundFee} " +
+            "  ,ORDER_FAIL_COUNT=(SELECT COUNT(1) FROM BIZ_ORDER WHERE BILL_CONTRAST_TYPE = '2' and date_format(str_to_date(left(DD_ZFSJ,10),'%Y-%m-%d'),'%Y%m%d') = #{billDate} ) " +
+            "  ,Bill_FAIL_COUNT=(SELECT COUNT(1) FROM BIZ_BILL_CONTRAST WHERE DISPOSE_TYPE = '2' AND TRADE_TIME= #{billDate}) " +
+            " WHERE PKID=#{bizTjDz.pkid} " +
+            " </script>"})
+    void updateStatistics(@Param("bizTjDz") BizTjDz bizTjDz, @Param("billDate") String billDate);
 }
