069963bc533374994f649b48c4bdca924ee08759
yangx <2003456>
2018-09-11 18:53:08 +0800

    学员的有效期 修改为永久有效 会员的有效期，修改为1年有效

--- biz/src/main/java/com/cwb/platform/biz/service/impl/OrderServiceImpl.java
+++ biz/src/main/java/com/cwb/platform/biz/service/impl/OrderServiceImpl.java
@@ -173,7 +173,6 @@ public class OrderServiceImpl extends BaseServiceImpl<BizOrder,java.lang.String>
 
 
 
-
         BizPtyh bizPtyh=new BizPtyh();
         bizPtyh.setId(order.getYhId());
         bizPtyh.setDdSfjx("1");
@@ -190,22 +189,28 @@ public class OrderServiceImpl extends BaseServiceImpl<BizOrder,java.lang.String>
             bizPtyh.setYhZsyqm(yhZsyqm);//用户自己邀请码
             bizPtyh.setYhZsyqmImg("/"+yhZsyqmImg+yhZsyqm + ".png");//用户自己邀请码
         }
+
+        long plusYears=1;
+        if(StringUtils.equals(bizCp.getCpType(),"1")){
+            plusYears=100;//学员的会员的有效期是永久的。
+        }
+
         // 增加二维码有效期
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime dateTime = LocalDateTime.now();
         LocalDateTime years = null;
         if(StringUtils.isBlank(queryUser.getYhYqmgqsj())){ // 邀请码过期时间为空 , 添加过期时间为一年
-            years = dateTime.plusYears(1L);
+            years = dateTime.plusYears(plusYears);
             bizPtyh.setYhYqmgqsj(years.format(formatter));
         }else {
 
             // 验证有效期是否已经过了
             if(queryUser.getYhYqmgqsj().compareTo(dateTime.format(formatter)) > 0){
                 LocalDateTime localDateTime = LocalDateTime.parse(queryUser.getYhYqmgqsj(),formatter);
-                 years = localDateTime.plusYears(1L);
+                 years = localDateTime.plusYears(plusYears);
                 bizPtyh.setYhYqmgqsj(years.format(formatter));
             }else {
-                 years = dateTime.plusYears(1L);
+                 years = dateTime.plusYears(plusYears);
                 bizPtyh.setYhYqmgqsj(years.format(formatter));
             }
         }
