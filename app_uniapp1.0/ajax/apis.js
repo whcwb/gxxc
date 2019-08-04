5a14ba6ad1164cd50e40bc9016ffc599ead9ffeb
youaretheappleinmyeyes <965567396@qq.com>
2019-08-04 16:10:47 +0800

    uniapp

---
+++ app_uniapp1.0/ajax/apis.js
@@ -0,0 +1,10 @@
+// let ajaxUrl = 'http://192.168.123.6:81';
+// let ajaxUrl = 'http://127.0.0.1:81';
+// let ajaxUrl = 'http://192.168.123.13:8080';//伟
+// let ajaxUrl = 'http://192.168.1.140:8080';
+let ajaxUrl = "http://www.520xclm.com";
+export default {
+	url: ajaxUrl+":8080/biz",
+	LOGIN:'/app/login',//登录接口
+
+}
---
+++ app_uniapp1.0/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App'
+
+import store from './store'
+Vue.prototype.$store = store
+
+import http from './ajax/index.js'
+Vue.prototype.$http = http
+
+import apis from './ajax/apis.js'
+Vue.prototype.apis = apis;
+
+// import iView from 'iview';
+// import 'iview/dist/styles/iview.css';
+// import 'common/iview.css'
+// Vue.use(iView);
+
+// import { Button } from 'iview';
+// Vue.component('Button', Button);
+
+
+Vue.config.productionTip = false
+
+
+App.mpType = 'app'
+
+const app = new Vue({
+    store,
+    ...App
+})
+app.$mount()
