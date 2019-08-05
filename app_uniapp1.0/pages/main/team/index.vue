b443c3334b8bb1832073059afdb43c3137d877d4
quan <1538452228@qq.com>
2019-08-05 23:09:12 +0800

    1

--- app_uniapp1.0/pages/main/team/index.vue
+++ app_uniapp1.0/pages/main/team/index.vue
@@ -5,7 +5,7 @@
 					<mSearch :mode="2" button="inside" @search="search($event,2)"></mSearch>
 				</view>
 				<view  style="width: 80px;">
-					筛选
+					<sl-filter :themeColor="themeColor" :menuList="menuList" @result="result"></sl-filter>
 				</view>
 			</view>
 			<view class="box_col_100">
@@ -54,7 +54,122 @@
 		data(){
 			return {
 				val0: '',
-				menuList:[]
+				themeColor: '#000000',
+                filterResult: '',
+                menuList: [{
+                        'title': '职位',
+                        'detailTitle': '请选择职位类型（可多选）',
+                        'isMutiple': true,
+                        'key': 'jobType',
+                        'detailList': [{
+                                'title': '不限',
+                                'value': ''
+                            },
+                            {
+                                'title': 'uni-app',
+                                'value': 'uni-app'
+                            },
+                            {
+                                'title': 'java开发',
+                                'value': 'java'
+                            },
+                            {
+                                'title': 'web开发',
+                                'value': 'web'
+                            },
+                            {
+                                'title': 'Android开发',
+                                'value': 'Android'
+                            },
+                            {
+                                'title': 'iOS开发',
+                                'value': 'iOS'
+                            }
+                        ]
+
+                    },
+                    {
+                        'title': '月薪',
+                        'key': 'salary',
+                        'isMutiple': true,
+                        'detailTitle': '请选择月薪范围（可多选）',
+                        'detailList': [{
+                                'title': '不限',
+                                'value': ''
+                            },
+                            {
+                                'title': '7000~8000',
+                                'value': '7000~8000'
+                            },
+                            {
+                                'title': '8000~9000',
+                                'value': '8000~9000'
+                            },
+                            {
+                                'title': '9000~10000',
+                                'value': '9000~10000'
+                            },
+                            {
+                                'title': '10000以上',
+                                'value': '10000~1000000'
+                            }
+                        ]
+
+                    },
+                    {
+                        'title': '单选',
+                        'key': 'single',
+                        'isMutiple': false,
+                        'detailTitle': '请选择（单选）',
+                        'detailList': [{
+                                'title': '不限',
+                                'value': ''
+                            },
+                            {
+                                'title': '条件1',
+                                'value': 'test_1'
+                            },
+                            {
+                                'title': '条件2',
+                                'value': 'test_2'
+                            },
+                            {
+                                'title': '条件3',
+                                'value': 'test_3'
+                            },
+                            {
+                                'title': '条件4',
+                                'value': 'test_4'
+                            },
+                            {
+                                'title': '条件5',
+                                'value': 'test_5'
+                            }
+                        ]
+                    },
+                    {
+                        'title': '排序',
+                        'key': 'sort',
+                        'isSort': true,
+                        'detailList': [{
+                                'title': '默认排序',
+                                'value': ''
+                            },
+                            {
+                                'title': '发布时间',
+                                'value': 'add_time'
+                            },
+                            {
+                                'title': '薪资最高',
+                                'value': 'wages_up'
+                            },
+                            {
+                                'title': '离我最近',
+                                'value': 'location'
+                            }
+                        ]
+                    }
+                ]
 			}
 		},
 		onShow(){
@@ -64,7 +179,11 @@
 			search(e, val) {
 				console.log(e, val);
 				this['val'+val] = e;
-			}
+			},
+			 result(val) {
+                console.log('filter_result:' + JSON.stringify(val));
+                this.filterResult = JSON.stringify(val, null, 2)
+            }
 		}
 	}
 </script>
