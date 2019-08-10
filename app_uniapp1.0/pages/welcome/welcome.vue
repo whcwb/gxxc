a80006775f232a849bf30da83f7123ca7e026fd2
youaretheappleinmyeyes <965567396@qq.com>
2019-08-11 00:13:21 +0800

    asd

--- app_uniapp1.0/pages/welcome/welcome.vue
+++ app_uniapp1.0/pages/welcome/welcome.vue
@@ -7,7 +7,6 @@
 
 <script>
 	import wLoading from "@/components/w-loading/w-loading.vue";
-	// var eshake = require("eshake");
 	export default {
 		components: {
 			wLoading
@@ -22,13 +21,13 @@
 		onReady() {
 			this.$refs.loading.open()
 			// #ifdef H5
-			 // this.getWxJs()
+			 this.getWxJs()
 			// #endif
 			// #ifdef APP-PLUS
 			// app执行
 			this.toLogin()
 			// #endif
-			this.toLogin()
+			// this.toLogin()
 		},
 		methods: {
 			toLogin() {
