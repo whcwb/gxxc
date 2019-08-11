2e4d5bc631e798370d818db43fb36da3d35a037c
youaretheappleinmyeyes <965567396@qq.com>
2019-08-11 18:42:36 +0800

    uniApp1.0

--- app_uniapp1.0/pages/welcome/welcome.vue
+++ app_uniapp1.0/pages/welcome/welcome.vue
@@ -42,13 +42,13 @@
 			// this.toLogin()
 			this.$refs.loading.open()
 			// #ifdef H5
-			 // this.getWxJs()
+			 this.getWxJs()
 			// #endif
 			// #ifdef APP-PLUS
 			// app执行
 			this.toLogin()
 			// #endif
-			this.toLogin()
+			// this.toLogin()
 		},
 		methods: {
 			toLogin() {
