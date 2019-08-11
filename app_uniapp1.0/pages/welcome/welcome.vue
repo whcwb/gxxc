fdb5a52661aebd9e172ea79339135de7407ee5ce
quan <1538452228@qq.com>
2019-08-11 17:31:45 +0800

    评价 ,  二维码跳转

--- app_uniapp1.0/pages/welcome/welcome.vue
+++ app_uniapp1.0/pages/welcome/welcome.vue
@@ -17,26 +17,7 @@
 			}
 		},
 		onShow(){
-			var url = location.search; //获取url中"?"符后的字串
-			  if (url.indexOf("?") != -1) {    //判断是否有参数
-				var str = url.substr(1); //从第一个字符开始 因为第0个是?号 获取所有除问号的所有符串
-				var strs = str.split("&");//用等号进行分隔 （因为知道只有一个参数 所以直接用等号进分隔 如果有多个参数 要用&号分隔 再用等号进行分隔）
-				var yam = strs[1].split('=');
-				var yqm = yam[1];
-				if(yqm!=''&&yqm!=null&&yqm!=undefined&&yam[0]=='id'){
-					uni.setStorage({
-						key: 'yqm',
-						data: yqm,
-						success: function () {
-							console.log('success');
-						}
-					});
-				  ui.navigateTo({
-					url: '../reg/reg'
-				  })
-
-				}          //直接弹出第一个参数 （如果有多个参数 还要进行循环的）
-			}
+			
 		},
 		onReady() {
 			// this.toLogin()
@@ -52,18 +33,42 @@
 		},
 		methods: {
 			toLogin() {
-				var  token = uni.getStorageSync('token');
-				setTimeout(()=>{
-					if(token){
-						uni.switchTab({
-							url: '/pages/main/p_index/main'
+				var url = location.search; //获取url中"?"符后的字串
+				 if (url.indexOf("?") != -1) {    //判断是否有参数
+					var str = url.substr(1); //从第一个字符开始 因为第0个是?号 获取所有除问号的所有符串
+					var strs = str.split("&");//用等号进行分隔 （因为知道只有一个参数 所以直接用等号进分隔 如果有多个参数 要用&号分隔 再用等号进行分隔）
+					var yam = strs[1].split('=');
+					var yqm = yam[1];
+					if(yqm!=''&&yqm!=null&&yqm!=undefined&&yam[0]=='id'){
+						uni.setStorage({
+							key: 'yqm',
+							data: yqm,
+							success: function () {
+								console.log('success');
+							}
 						});
+						setTimeout(()=>{
+							 ui.navigateTo({
+								url: '../reg/reg'
+							})
+						})
+					 
 					}else{
-						uni.navigateTo({
-							url: '../login/login'
-						});
+						var  token = uni.getStorageSync('token');
+						setTimeout(()=>{
+							if(token){
+								uni.switchTab({
+									url: '/pages/main/p_index/main'
+								});
+							}else{
+								uni.navigateTo({
+									url: '../login/login'
+								});
+							}
+						},3000)
 					}
-				},3000)
+				}
+				
 			},
 			getWxJs() {
 				var v = this
