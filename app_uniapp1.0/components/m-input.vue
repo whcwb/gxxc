c7ec031016d7022ffb3edfed0ac77f2471a2d1bb
quan <1538452228@qq.com>
2019-08-27 23:57:43 +0800

    绑定指南

--- app_uniapp1.0/components/m-input.vue
+++ app_uniapp1.0/components/m-input.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="m-input-view">
 		<input :focus="focus_" :type="inputType" :value="value" @input="onInput" class="m-input-input" :placeholder="placeholder"
-		 :password="type==='password'&&!showPassword" @focus="onFocus" @blur="onBlur" style="font-size: 24px;height: 30px;margin: 10px 0;" />
+		 :password="type==='password'&&!showPassword" @focus="onFocus" @blur="onBlur"/>
 		<!-- 优先显示密码可见按钮 -->
 		<view v-if="clearable_&&!displayable_&&value.length" class="m-input-icon">
 			<m-icon color="#666666" type="clear" size="20" @click="clear"></m-icon>
--- app_uniapp1.0/pages/login/login.vue
+++ app_uniapp1.0/pages/login/login.vue
@@ -1,7 +1,7 @@
 <template>
 	<view style="width: 100%;padding-top: 1upx;background: rgba(255,255,255,1);">
 		<view style="margin: 102upx 0 140upx;text-align: center;">
-			<img src="/static/img/head.png" style="width:136upx;height: 136upx;">
+			<img src="/static/img/head.png" style="width:192upx;height: 192upx;">
 		</view>
 		<!-- <view>infoRes:{{infoRes}}</view> 
 		<view>loginRes:{{loginRes}}</view> -->
@@ -10,7 +10,7 @@
 			<input class="uni-input input" style="font-size: 16px;height: 40px;margin: 10px 30rpx;" password placeholder="请输入密码" v-model="form.password" />
 		</view>
 		<view style="margin-bottom: 136upx;text-align: right;width: 678upx;font-size:28rpx;color:rgba(37,128,222,1);" @click="topwd">忘记密码？</view>
-		<view class="btn" @tap="login">
+		<view class="copybtn" @tap="login">
 			登录
 		</view>
 		<!-- <view class="btn" @tap="wxlogin">
@@ -19,7 +19,7 @@
 		<view class="btn" @tap="logout">
 			退出微信登录
 		</view> -->
-		<view class="createAccount" @tap="toReg">
+		<view class="sharebtn" @tap="toReg">
 			创建账号
 		</view>
 	</view>
@@ -110,6 +110,32 @@
 </script>
 
 <style>
+	.copybtn {
+		width:70%;
+		height: 80upx;
+		background: linear-gradient(132deg, rgba(59, 147, 253, 1) 0%, rgba(60, 128, 253, 1) 41%, rgba(55, 84, 252, 1) 100%);
+		box-shadow: 0 8upx 16upx 0 rgba(69, 124, 232, 0.5);
+		border-radius: 52upx;
+		margin: 40upx auto 28upx;
+		font-size: 32upx;
+		font-weight: 400;
+		color: rgba(255, 255, 255, 1);
+		line-height: 80upx;
+		text-align: center;
+	}
+	
+	.sharebtn {
+		width: 70%;
+		height: 80upx;
+		box-shadow: 0 8upx 16upx 0 rgba(69, 124, 232, 0.5);
+		border-radius: 52upx;
+		border: 2upx solid rgba(151, 151, 151, 1);
+		margin: 0 auto;
+		font-size: 32upx;
+		font-weight: 400;
+		line-height: 80upx;
+		text-align: center;
+	}
 	.inputMess {
 		width: 100%;
 		background: rgba(255, 255, 255, 1);
