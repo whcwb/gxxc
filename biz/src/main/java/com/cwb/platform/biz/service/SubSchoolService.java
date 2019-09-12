d0906031adcc40cbdb96c6dff3429f6e0d06e91b
38492 <384920400@qq.com>
2019-09-12 23:33:10 +0800

    update

--- biz/src/main/java/com/cwb/platform/biz/service/SubSchoolService.java
+++ biz/src/main/java/com/cwb/platform/biz/service/SubSchoolService.java
@@ -2,6 +2,8 @@ package com.cwb.platform.biz.service;
 
 import com.cwb.platform.biz.model.BizSubSchool;
 import com.cwb.platform.sys.base.BaseService;
+import com.cwb.platform.util.bean.ApiResponse;
 
 public interface SubSchoolService extends BaseService<BizSubSchool,String> {
+    ApiResponse<String> getOpenid(String phone);
 }
