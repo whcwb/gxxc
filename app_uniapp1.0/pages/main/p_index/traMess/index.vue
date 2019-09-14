dfa72cacc8a50cf79698bb936fffcd7eb4f69ce7
youaretheappleinmyeyes <965567396@qq.com>
2019-09-14 16:53:55 +0800

    uniApp1.0

---
+++ app_uniapp1.0/pages/main/p_index/traMess/index.vue
@@ -0,0 +1,80 @@
+<template>
+	<view class="reaBox">
+		<img class="traImg" :src="traMess.placeCoverImg.split(',')[0]" alt="">
+		<view class="schoolName">
+			{{traMess.schoolName}}
+		</view>
+		<view class="address">
+			{{traMess.address}}
+		</view>
+		<view class="lineRow"></view>
+		<view class="imgBoxTit">
+			相册
+		</view>
+		<view class="lineRow"></view>
+		<view class="imgBox">
+			<view class="imgItem" v-for="(it,index) in traMess.placeImg.split(',')">
+				<img class="traImg" :src="it" alt="">
+			</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "tra",
+		computed: {
+			traMess() {
+				return this.$store.state.traMess
+			}
+		},
+		data(){
+			return {
+					urlImg:this.apis.getImgUrl,
+			}
+		},
+		onShow(){
+			// placeImg
+			console.log(this.traMess)
+			uni.setNavigationBarTitle({
+				title: this.traMess.placeName
+			});
+		}
+	}
+</script>
+
+<style lang="less">
+	.padd{
+		padding: 10rpx 40rpx;
+	}
+	.reaBox{
+		width: 100%;
+		.traImg{
+			width: 100%;
+			height: 300rpx;
+		}
+		.schoolName{
+			.padd;
+			font-size: 48rpx;
+		}
+		.address{
+			.padd;
+			font-size: 30rpx;
+		}
+		.lineRow{
+			margin: 0 40rpx;
+			height: 2rpx;
+			width: 100%;
+			background-color: #bfbfbf;
+		}
+		.imgBoxTit{
+			font-size: 32rpx;
+			font-weight: 600;
+			.padd;
+		}
+		.imgBox{
+			.padd;
+		}
+	}
+</style>
